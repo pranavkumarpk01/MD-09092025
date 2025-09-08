d1fdce0e53e83368ae6e92c9901812cbca7dc346
Ubuntu <ubuntu@ip-172-31-41-123.ap-south-1.compute.internal>
2025-09-08 02:49:55 +0000

    saving a foloop file

---
+++ forloop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+# Ask for user input
+echo "Enter a number:"
+read n
+
+# Loop from 1 to N
+for ((i=100; i>=n; i--)); do
+    echo "Number: $i"
+done
+
+
+eco " This is a dummy line"
